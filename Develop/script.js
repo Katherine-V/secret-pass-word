9e0a46084bfe1efa28eb130f26cdd46d998cad2c
Katherine V <msvogler919@gmail.com>
2023-09-23 20:34:13 -0400

    adding from notes to starter code

--- Develop/script.js
+++ Develop/script.js
@@ -5,17 +5,36 @@
 
 
 
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var length=[]
+var upperCase="ABCDEFGH"
+var lowerCase="abcdefgh"
+var number="0123456789"
+var symbol="   "
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var allChars= lowerCase + upperCase + number + symbol;
+  
+  
+  //[Math.floor(Math.random())]
 
   passwordText.value = password;
+  let password = ""
 
-}
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+    
+    while (length > password.length){
+      password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Password Generator Starter Code
+#Random letter and/or number and/or symbol generator
+
+Users:
+For the use of those that need a randomized phrase composed of their choice of letters, numbers, and symbols.
+
+Functions:
+Enter parameters such as symbols, letters, and/or numbers and set a length for this phrase. Passwords are created with user input. 
+
+sources:
+EdEx web development program starter code for Password Generator project
+
+YouTube "How to Generate a Random Password Using JavaScript. Posted by account named "GreatStack" with 891k subscribers. Video updated JApril 20 202023 as the #javascriptproject "30 days 30 JAvascript projects for beginners to practice"
